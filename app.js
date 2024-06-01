b729fcb4c45d8400f098e562f3965949332797a9
Muneeb-Shahabhassan <muneebsahabhassan098890@gmail.com>
2024-06-01 11:07:18 -0700

    11.NAMES

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let members = [`Azam`, `Zaid`, `Muneeb`, `Rafay`, `Mubashir`];
+for (let i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ app.ts
@@ -0,0 +1,4 @@
+let members: string[] =[`Azam`,`Zaid`,`Muneeb`,`Rafay`,`Mubashir`]
+for(let i=0; i<members.length; i++){
+    console.log(members[i]);
+}
